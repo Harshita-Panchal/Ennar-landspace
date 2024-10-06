a6f0b93780dd4d31e77e3871764f96f6a29a0f82
Harshita Panchal <harshita.panchal@envsage.com>
2024-10-06 17:35:24 +0530

    WIP home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary-green': '#737B52',
-				'secondary-green': '#2B2B2B',
-			  },
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-green": "#737B52",
+        "secondary-green": "#2B2B2B",
+        "light-taupe": "#D8CDAF",
+        "soft-terracotta": "#BA7464",
+        "ghost-white": "#F8F8FF",
+      },
+    },
+  },
+  plugins: [],
+};
